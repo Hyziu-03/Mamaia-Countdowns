c85eff3b6959e94b2684760a0c284c6ab46238bf
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-10-14 20:16:39 +0200

    Explanation Addition

---
+++ mamaia-countdowns/src/comps/Explanation.js
@@ -0,0 +1,12 @@
+const Explanation = (props) => {
+    return (
+        <article className="text-section">
+            <h1 className="heading" tabIndex='0'>{props.heading}</h1>
+            <p className="description" tabIndex='0'>{props.description[0]}</p>
+            <p className="description" tabIndex='0'>{props.description[1]}</p>
+            <p className="description" tabIndex='0'>{props.description[2]}</p>
+        </article>
+    )
+}
+
+export default Explanation;

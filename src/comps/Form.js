b9cf7cf20abce0c02bec9f5ccef25c94fa2862e2
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-10-15 15:41:58 +0200

    Form Addition

---
+++ src/comps/Form.js
@@ -0,0 +1,24 @@
+import Button from "../items/Button";
+import Background from '../img/form-background.svg';
+
+const Form = (props) => {
+    return (
+        <article className="article">
+            <section className="request">
+                <h1 className="heading" tabIndex='0'>{props.heading}</h1>
+                <p className="description" tabIndex='0'>{props.description[0]}</p>
+                <p className="description" tabIndex='0'>{props.description[1]}</p>
+                <p className="description" tabIndex='0'>{props.description[2]}</p>
+            </section>
+            <form action="" className="form">
+                <input type="text" name="text-input" id="text-input" className="input" placeholder="What is your name?"/>
+                <input type="email" name="email-input" id="email-input" className="input" placeholder="What is your email?"/>
+                <textarea placeholder="What do you want to say?" className="input textarea"/>
+                <Button message="Send Message!"/>
+            </form>
+            <Background />
+        </article>
+    )
+}
+
+export default Form;

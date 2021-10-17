087f1cf5238e27685449db8301986d531d78e9d0
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-10-17 12:30:57 +0200

    Security Enhancement

--- src/comps/Footer.js
+++ src/comps/Footer.js
@@ -3,9 +3,9 @@ const Footer = () => {
         <footer className="footer">
             <p className="copyright">This app has been created from scratch by Szymon Hyziak and therefore is upon copyright of the owner.</p>
             <section className="contact-links">
-                <a href='https://www.linkedin.com/in/szymon-hyziak/' target="_blank"><i class="fab fa-linkedin-in"></i></a>
-                <a href='https://github.com/Hyziu-03/' target="_blank"><i class="fab fa-github"></i></a>
-                <i class="fab fa-paypal" disabled></i>
+                <a href='https://www.linkedin.com/in/szymon-hyziak/' target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in"></i></a>
+                <a href='https://github.com/Hyziu-03/' target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a>
+                <i className="fab fa-paypal" disabled></i>
             </section>
         </footer>
     )
--- src/comps/Introduction.js
+++ src/comps/Introduction.js
@@ -7,7 +7,7 @@ const Introduction = (props) => {
                 <h1 className="heading" tabIndex='0'>{props.heading}</h1>
                 <p className="description" tabIndex='0'>{props.description}</p>
             </section>
-            <Button message="Set a Countdown!"/>
+            <Button message="Set a Countdown!" type='navigate'/>
         </article>
     )
 }

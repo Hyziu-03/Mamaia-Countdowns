6b57f4c0a6d5bde88852f61b1ede475752294af9
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-10-17 12:30:46 +0200

    Typo Fix

--- src/items/Name.js
+++ src/items/Name.js
@@ -1,4 +1,4 @@
 const refresh = () => window.location.reload(true);
 
-const Name = () => <h1 className="name hover" tabIndex='0' onClick={refresh}><i class="far fa-calendar-alt icon"></i>&nbsp;Mamaia Countdowns</h1>
+const Name = () => <h1 className="name hover" tabIndex='0' onClick={refresh}><i className="far fa-calendar-alt icon"></i>&nbsp;Mamaia Countdowns</h1>
 export default Name;

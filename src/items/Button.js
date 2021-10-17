35b1fb8bc4e08e0f8d3fc9e58ce95305ce650d57
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-10-17 12:29:18 +0200

    Form Validation

--- src/items/Button.js
+++ src/items/Button.js
@@ -1,2 +1,20 @@
-const Button = (props) => <button className="btn hover" tabIndex='0'>{props.message}</button>
+import * as EmailValidator from 'email-validator';
+
+const validate = () => {
+    let isSent = false;
+    const emailInput = document.getElementById('email-input');
+    const textInput = document.getElementById('text-input');
+    const message = document.getElementById('textarea');
+
+    let isNotCorrect = EmailValidator.validate(emailInput.value) === false || textInput.value === '' || message.value === '';
+    isNotCorrect ? alert('Please, fill in all the information requested.') : isSent = true;
+
+    if(isSent) {
+        emailInput.value = ''; textInput.value = ''; message.value = '';
+        alert('Your message has been sent successfully!');
+    }
+}
+
+const Button = (props) => <button className="btn hover" tabIndex='0' onClick={(event) => {event.preventDefault(); validate();}}>{props.message}</button>
+
 export default Button;

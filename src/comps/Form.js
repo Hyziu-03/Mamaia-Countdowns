7923611371b169492400da27942db1d6bce68973
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-10-20 19:16:55 +0200

    Form Correction

--- src/comps/Form.js
+++ src/comps/Form.js
@@ -1,7 +1,15 @@
+import { useForm, ValidationError } from '@formspree/react';
+
 import Button from "../items/Button";
 import Background from '../img/Background';
 
 const Form = (props) => {
+    const [state, handleSubmit] = useForm("xdoyvago");
+
+    if(state.succeeded) {
+        console.log('The from has been successfully sent.');
+    }
+
     return (
         <article className="article">
             <section className="request">
@@ -10,11 +18,11 @@ const Form = (props) => {
                 <p className="description" tabIndex='0'>{props.description[1]}</p>
                 <p className="description" tabIndex='0'>{props.description[2]}</p>
             </section>
-            <form action="https://formspree.io/f/xdoyvago" method="POST" className="form" id='form'>
+            <form action="https://formspree.io/f/xdoyvago" method="POST" className="form" id='form' onSubmit={handleSubmit}>
                 <input type="text" name="name" id="text-input" className="input" placeholder="What is your name?"/>
-                <input type="email" name="_replyto" id="email-input" className="input" placeholder="What is your email?"/>
-                <textarea placeholder="What do you want to say?" className="input textarea" id="textarea" name='message'/>
-                <Button message="Send Message!"/>
+                <input type="email" name="email" id="email" className="input" placeholder="What is your email?"/>
+                <textarea placeholder="What do you want to say?" className="input textarea" id="message" name='message'/>
+                <Button message="Send Message!" origin='form'/>
             </form>
             <Background />
         </article>

dd580d83780556fdcac1f5155d704af85587677a
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-10-24 13:01:09 +0200

    Accessibility Enhancement

--- src/items/Name.js
+++ src/items/Name.js
@@ -1,4 +1,4 @@
 const name = window.innerWidth < 950 ? 'Mamaia' : 'Mamaia Countdowns';
 
-const Name = () => <a href=''><h1 className="name hover"><i className="far fa-calendar-alt icon"></i>&nbsp;{name}</h1></a>
+const Name = () => <a href='index.html'><h1 className="name hover"><i className="far fa-calendar-alt icon"></i>&nbsp;{name}</h1></a>
 export default Name;

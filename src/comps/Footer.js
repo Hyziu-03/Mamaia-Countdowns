68d8b287bed6d6d25de3086fba4b3c5a60696576
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-11-02 15:10:41 +0100

    Code Formatting

--- src/comps/Footer.js
+++ src/comps/Footer.js
@@ -3,8 +3,12 @@ const Footer = () => {
         <footer className="footer">
             <p className="copyright" tabIndex='0'>This app has been created from scratch by Szymon Hyziak and therefore is upon copyright of the owner.</p>
             <section className="contact-links">
-                <a href='https://www.linkedin.com/in/szymon-hyziak/' target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in touch-target"></i></a>
-                <a href='https://github.com/Hyziu-03/' target="_blank" rel="noreferrer"><i className="fab fa-github touch-target"></i></a>
+                <a href='https://www.linkedin.com/in/szymon-hyziak/' target="_blank" rel="noreferrer">
+                    <i className="fab fa-linkedin-in touch-target"></i>
+                </a>
+                <a href='https://github.com/Hyziu-03/' target="_blank" rel="noreferrer">
+                    <i className="fab fa-github touch-target"></i>
+                </a>
                 <i className="fab fa-paypal" disabled></i>
             </section>
         </footer>
--- src/items/Name.js
+++ src/items/Name.js
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 
 const name = window.innerWidth < 950 ? 'Mamaia' : 'Mamaia Countdowns';
-const refresh = () => window.location.reload(false);
 
-const Name = () => <Link to='/' onClick={refresh}><h1 className="name hover touch-target"><i className="far fa-calendar-alt icon"></i>&nbsp;{name}</h1></Link>
+const Name = () => {
+    return (
+        <Link to='/'>
+            <h1 className="name hover touch-target"><i className="far fa-calendar-alt icon"></i>&nbsp;{name}</h1>
+        </Link>    
+    )
+}
 export default Name;

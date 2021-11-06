60cf0c6d584d376e441beb94ec4add1a27d7c4f9
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-11-06 13:48:15 +0100

    Local Storage API Addition

--- README.md
+++ README.md
@@ -9,21 +9,18 @@ Mamaia Countdowns is a tool to ease your life. You can set countdowns for any ev
 ### What does not work well>
 
 - Correct readability od mamaia.sass,
-- Correct readability od mamaia.js,
-- Correct CSS of the Mobile component,
+- Correct readability od mamaia.js.
 
 ### Which technologies are yet to be used?
 
-- Use the Notification API,
-- Use the Local Storage API,
+- Use the Notification API.
 
 ### What is to correct about SEO?
 
 - Check for broken images, videos, and descriptive alt texts,
 - Check for broken links, h-tags, and cannibalisation,
-- Ensure clear description of services, products, or purpose,
+- Ensure clear description of services, products, or purpose.
 
 ### What is to correct about file structure?
 
-- Remove unused code from index.sass,
-- Delete mamaia.min.css and prevent it from showing up again,
+- Remove unused code from index.sass.
--- src/comps/Mobile.js
+++ src/comps/Mobile.js
@@ -3,14 +3,46 @@ import Contact from "../items/Contact";
 import Button from "../items/Button";
 import { refresh } from '../libraries/mamaia';
 
+const loadEvents = () => {
+    // ? Initialise the number of events when there aren't any of them:
+
+    if(localStorage.getItem('totalNumber') === null) {
+        localStorage.setItem('totalNumber', 0);
+    }
+
+    
+
+    if(parseInt(localStorage.getItem('totalNumber')) > 0) {
+        for(let counter = 0; counter < localStorage.getItem('totalNumber'); counter += 1) {
+            localStorage.getItem('event' + counter + 'name');
+            localStorage.getItem('event' + counter + 'date');
+            localStorage.getItem('event' + counter + 'description');
+        }
+    }
+}
+
 let emailInput = document.getElementsByClassName('email');
 let nameInput = document.getElementsByClassName('text-input');
 let descriptionInput = document.getElementsByClassName('message');
 
 const handleSubmit = () => {
+    // ? Put all of the entered data back to the user: 
+
     document.getElementById('events-name').innerHTML = nameInput[1].value;
-    document.getElementById('events-date').innerHTML = emailInput[1].value;
+    document.getElementById('events-date').innerHTML = 'This event will take place on ' + emailInput[1].value + '!';
     document.getElementById('events-description').innerHTML = descriptionInput[1].value;
+
+    // ? Save the new number of events in the local storage:
+
+    let currentNumber = parseInt(localStorage.getItem('totalNumber'));
+    localStorage.setItem("totalNumber", ++currentNumber);
+
+    // ? Save all of the new data in the local storage:
+
+    let newEventNumber = localStorage.getItem('totalNumber');
+    localStorage.setItem('event' + newEventNumber + 'name', nameInput[1].value);
+    localStorage.setItem('event' + newEventNumber + 'date', emailInput[1].value);
+    localStorage.setItem('event' + newEventNumber + 'description', descriptionInput[1].value);
 }
 
 const Mobile = () => {
@@ -18,6 +50,10 @@ const Mobile = () => {
     let name = nameInput.length > 1 && nameInput[1].value ? nameInput[1].value : "The event's name will show up here when you submit it.";
     let description = descriptionInput.length > 1 && descriptionInput[1].value ? descriptionInput[1].value : "Additional information about the event will show up here when you submit it.";
     
+    const validate = () => emailInput[1].value === '' || nameInput.value === '' || descriptionInput.value === '' ? alert('Please, fill in all the information requested.') : handleSubmit();
+
+    loadEvents();
+
     return (
         <div className="mobile-container">
 
@@ -34,7 +70,7 @@ const Mobile = () => {
                         secondInputType = 'date'
                         thirdInput='What additional information do you have?'
                     />
-                    <span onClick={handleSubmit}>
+                    <span onClick={validate}>
                         <Button message="Set a Countdown!" />
                     </span>
                 </article>

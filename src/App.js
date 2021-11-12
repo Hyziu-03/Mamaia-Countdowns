fcb5167a2673724b932d021ef64ed7d2869f87bb
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-11-12 16:30:17 +0100

    Service Worker Registration

--- src/App.js
+++ src/App.js
@@ -20,8 +20,20 @@ import Creator from './img/creator.webp';
 import { integrateTabIndex } from "./libraries/reusable";
 integrateTabIndex();
 
-if(typeof window.ethereum.autoRefreshOnNetworkChange !== "undefined") { 
-  window.ethereum.autoRefreshOnNetworkChange = false; 
+if (typeof window.ethereum.autoRefreshOnNetworkChange !== "undefined") {
+  window.ethereum.autoRefreshOnNetworkChange = false;
+}
+
+// ? Service Worker Registration:
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw.js').then(function (registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
 }
 
 function App() {

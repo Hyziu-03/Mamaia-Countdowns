ab45002ecf493cbd0bc41d95ab914519eaaa60d1
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-11-12 16:52:25 +0100

    Version 1.0 Build

--- src/comps/Mobile.js
+++ src/comps/Mobile.js
@@ -6,61 +6,89 @@ import { refresh } from '../libraries/reusable';
 
 // ? Initialise event counter on first load:
 
-if(localStorage.getItem('totalNumber') === null) {
-    localStorage.setItem('totalNumber', 1);
-    localStorage.setItem('event0name', "The event 's name will show up here when you submit it.");
-    localStorage.setItem('event0date', "Your date will show up here when you submit it.");
-    localStorage.setItem('event0description', "Additional information about the event will show up here when you submit it.");
-}  
+try {
+    if (typeof window !== 'undefined') {
+        if (localStorage.getItem('totalNumber') === null) {
+            localStorage.setItem('totalNumber', 1);
+            localStorage.setItem('event0name', "The event 's name will show up here when you submit it.");
+            localStorage.setItem('event0date', "Your date will show up here when you submit it.");
+            localStorage.setItem('event0description', "Additional information about the event will show up here when you submit it.");
+        }
+    }
+} catch(error) {
+    throw new Error(error);
+}
 
 let emailInput = document.getElementsByClassName('email');
 let nameInput = document.getElementsByClassName('text-input');
 let descriptionInput = document.getElementsByClassName('message');
 
 const handleSubmit = () => {
-    // ? Save the new number of events to the local storage:
+    try {
+        if (typeof window !== 'undefined') {
+            // ? Save the new number of events to the local storage:
 
-    let currentNumber = parseInt(localStorage.getItem('totalNumber'));
-    localStorage.setItem("totalNumber", ++currentNumber);
+            let currentNumber = parseInt(localStorage.getItem('totalNumber'));
+            localStorage.setItem("totalNumber", ++currentNumber);
 
-    // ? Save all of the new data to the local storage:
+            // ? Save all of the new data to the local storage:
 
-    let newEventNumber = localStorage.getItem('totalNumber') - 1;
-    localStorage.setItem('event' + newEventNumber + 'name', nameInput[1].value);
-    localStorage.setItem('event' + newEventNumber + 'date', 'This event will happen on ' + emailInput[1].value);
-    localStorage.setItem('event' + newEventNumber + 'description', descriptionInput[1].value);
+            let newEventNumber = localStorage.getItem('totalNumber') - 1;
+            localStorage.setItem('event' + newEventNumber + 'name', nameInput[1].value);
+            localStorage.setItem('event' + newEventNumber + 'date', 'This event will happen on ' + emailInput[1].value);
+            localStorage.setItem('event' + newEventNumber + 'description', descriptionInput[1].value);
 
-    alert('Your event has been saved!');
+            alert('Your event has been saved!');
+        }
+    } catch(error) {
+        throw new Error(error);
+    }
 }
 
 const Mobile = () => {
     let [eventNumber, update] = useState(0);
 
-    const validate = () => emailInput[1].value === '' || nameInput.value === '' || descriptionInput.value === '' ? alert('Please, fill in all the information requested.') : handleSubmit();
-
-    const correctState = (direction) => {
-        // ? Check if the current event number is valid, correct it if it is not:
-
-        let valid = true;
-
-        if(eventNumber < 0) {
-            valid = false;
-        } 
-
-        if(eventNumber > localStorage.getItem('totalNumber') - 1) {
-            valid = false;
-        }
-
-        if(!valid) {
-            if(direction === 'right') {
-                update(--eventNumber);
-            } 
-
-            if(direction === 'left') {
-                update(++eventNumber);
-            }
+    const validate = () => {
+        try {
+            emailInput[1].value === '' || nameInput.value === '' || descriptionInput.value === '' ? alert('Please, fill in all the information requested.') : handleSubmit();
+        } catch(error) {
+            throw new Error(error);
         }
     }
+
+    const correctState = (direction) => {
+        try {
+            if (typeof window !== 'undefined') {
+                // ? Check if the current event number is valid, correct it if it is not:
+
+                let valid = true;
+
+                if (eventNumber < 0) {
+                    valid = false;
+                }
+
+                if (eventNumber > localStorage.getItem('totalNumber') - 1) {
+                    valid = false;
+                }
+
+                if (!valid) {
+                    if (direction === 'right') {
+                        update(--eventNumber);
+                    }
+
+                    if (direction === 'left') {
+                        update(++eventNumber);
+                    }
+                }
+            }
+        } catch(error) {
+            throw new Error(error);
+        }
+    }
+
+    let renderedName = localStorage.getItem('event' + eventNumber + 'name') || '';
+    let renderedDate = localStorage.getItem('event' + eventNumber + 'date') || '';
+    let renderedDescription = localStorage.getItem('event' + eventNumber + 'description') || '';
 
     return (
         <div className="mobile-container">
@@ -85,9 +113,9 @@ const Mobile = () => {
 
                 <article className="saved-countdowns">
 
-                    <h1 className="heading" id='events-name'>{localStorage.getItem('event' + eventNumber + 'name')}</h1>
-                    <p className="description" id='events-date'>{localStorage.getItem('event' + eventNumber + 'date')}</p>
-                    <p className="description" id='events-description'>{localStorage.getItem('event' + eventNumber + 'description')}</p>
+                    <h1 className="heading" id='events-name'>{renderedName}</h1>
+                    <p className="description" id='events-date'>{renderedDate}</p>
+                    <p className="description" id='events-description'>{renderedDescription}</p>
 
                     <section className="btn-container">
                         <button className="arrow-btn" onClick={() => {update(--eventNumber); correctState('left');}}><i className="fas fa-long-arrow-alt-left"></i></button>

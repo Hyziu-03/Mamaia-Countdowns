72f83ca77ef2517d4cc4df5bd9e6111d4e2504b5
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-11-27 12:14:39 +0100

    Email Sending Bug Fixes

--- src/items/Button.js
+++ src/items/Button.js
@@ -1,45 +1,38 @@
 import * as EmailValidator from 'email-validator';
 import DOMPurify from 'dompurify';
 
-var emailInput = undefined;
-var textInput = undefined;
-var messageInput = undefined;
-
-window.addEventListener('DOMContentLoaded', () => {
-    emailInput = document.getElementById('email')[0];
-    textInput = document.getElementById('text-input')[0];
-    messageInput = document.getElementById('message')[0];
-});
-
 const Button = (props) => {
-    const validateMessage = () => {
+    const prepareEmail = () => {
         try {
-            if (props.origin === 'form') {
+
+            if(props.origin === 'form') {
+
+                let username = document.getElementsByClassName('text-input')[0];
+                let email = document.getElementsByClassName('email')[0];
+                let message = document.getElementsByClassName('message')[0];
 
                 let isSent = false;
-                let isNotCorrect = EmailValidator.validate(emailInput.value) === false || textInput.value === '' || messageInput.value === '';
+                let isNotCorrect = EmailValidator.validate(email.value) === false || username.value === '' || message.value === '';
                 isNotCorrect ? alert('Please, fill in all the information requested.') : isSent = true;
 
-                if (isSent) {
-                    
+                if(isSent) {
+
                     // eslint-disable-next-line
-                    let emailInformation = 'mailto:szymon.hyziak@protonmail.com?' + 'subject=' + DOMPurify.sanitize(textInput.value) + ' | ' + DOMPurify.sanitize(emailInput.value) + '&body=' + DOMPurify.sanitize(messageInput.value);
-                    window.location.assign(emailInformation);
-
-                    emailInput.value = '';
-                    textInput.value = '';
-                    messageInput.value = '';
-
+                    let emailData = 'mailto:szymon.hyziak@protonmail.com?' + 'subject=' + DOMPurify.sanitize(username.value) + ' from ' + DOMPurify.sanitize(email.value) + '&body=' + DOMPurify.sanitize(message.value);
+                    window.location.assign(emailData);
                     alert('Please wait for redirection to your mail app.');
+                
                 }
+
             }
-        } catch(exception) {
-            throw new Error(exception);
+
+        } catch(error) {
+            throw new Error(error);
         }
     }
 
     return (
-        <button className="btn hover touch-target" tabIndex='-1' onClick={validateMessage} type='submit'>{props.message}</button>
+        <button className="btn hover touch-target" tabIndex='-1' type='submit' onClick={prepareEmail}>{props.message}</button>
     )
 }
 

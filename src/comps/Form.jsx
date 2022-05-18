673b0b4a4cad163c8c95ad1d7fa169f7f29907f7
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-05-18 14:06:42 +0200

    Applying additional changes

---
+++ src/comps/Form.jsx
@@ -0,0 +1,26 @@
+import Button from '../items/Button';
+import Contact from '../items/Contact';
+
+const Form = (props) => {
+    return (
+        <article className='article'>
+            <section className='request'>
+                <h1 className='heading' tabIndex='0'>{props.heading}</h1>
+                <p className='description' tabIndex='0'>{props.description[0]}</p>
+                <p className='description' tabIndex='0'>{props.description[1]}</p>
+                <p className='description' tabIndex='0'>{props.description[2]}</p>
+            </section>
+            <section className='contact-form-container'>
+                <Contact
+                    firstInput='What is your name?'
+                    secondInput='What is your email?'
+                    secondInputType='email'
+                    thirdInput='What do you want to say?'
+                />
+                <Button origin='form' message='Send a Message!' />
+            </section>
+        </article>
+    )
+}
+
+export default Form; 
--- src/libraries/reusable.js
+++ src/libraries/reusable.js
@@ -1,11 +1,9 @@
 export const integrateTabIndex = () => {
     document.addEventListener('keydown', (event) => {
         try {
-            
             if (event.key === 13) {
                 document.activeElement.click();
             }
-            
         } catch (exception) {
             throw new Error(exception);
         }

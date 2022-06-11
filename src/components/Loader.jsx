448954ed55943e1c85768385227936439184aa8c
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-06-11 11:23:00 +0200

    Formatting files

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,3 @@
-const Loader = () => {
-  return (
-    <section className='loader-container'>
-        <span className='loader'></span>
-    </section>
-  );
-}
-
+const Loader = () => <section className='loader-container'><span className='loader'></span></section>;
+  
 export default Loader;
--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -2,9 +2,6 @@ import { refresh } from '../scripts/scripts';
 
 const displayName = window.innerWidth < 950 ? 'Mamaia' : 'Mamaia Countdowns';
 
-const Name = () => {
-    return (
-        <h1 onClick={refresh} className='name hover touch-target'><span className='icon'>event_note</span>&nbsp;{displayName}&trade;</h1>
-    );
-}
+const Name = () => <h1 onClick={refresh} className='name hover touch-target' tabIndex='0'><span className='icon'>event_note</span>&nbsp;{displayName}&trade;</h1>;
+
 export default Name;

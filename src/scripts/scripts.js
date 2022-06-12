1eb8cc1e1827123da15494d0bbbaab6f7ca12aaa
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-06-12 11:45:31 +0200

    Modernising the code

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,7 +1,7 @@
 export const integrateTabIndex = () => {
     document.addEventListener('keydown', (event) => {
         try {
-            if (event.key === 13) document.activeElement.click();
+            event.key === 13 && document.activeElement.click();
         } catch (exception) {
             throw new Error(exception);
         }

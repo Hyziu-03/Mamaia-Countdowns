580d38bd81e579920d70a3a5a910fb11e31f6d08
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-06-16 13:59:39 +0200

    Using CSS modules

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,11 @@
+import '../styles/contact.css';
+
 const Contact = (props) => {
     return (
-        <form action='' method='' className='form article' id='form' onSubmit={(event) => event.preventDefault()} autoComplete='off'>
-            <input type='text' name='name' id='text-input' className='input touch-target text-input' placeholder={props.firstInput}/>
-            <input type={props.secondInputType} name='email' id='email' className='input touch-target email' placeholder={props.secondInput}/>
-            <textarea placeholder={props.thirdInput} className='input textarea touch-target message' id='message' name='message'/>
+        <form action="" method="" className="form article" id="form" onSubmit={(event) => event.preventDefault()}>
+            <input type="text" name="name" id="text-input" className="input touch-target text-input" placeholder={props.firstInput}/>
+            <input type={props.secondInputType} name="email" id="email" className="input touch-target email" placeholder={props.secondInput}/>
+            <textarea placeholder={props.thirdInput} className="input textarea touch-target message" id="message" name="message"/>
         </form>
     );
 }
--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -1,10 +1,12 @@
+import '../styles/paragraph.css';
+
 const Paragraph = (props) => {
   return (
     <article className={props.className}>
-        <h1 className='heading' tabIndex='0'>{props.heading}</h1>
-        <p className='description' tabIndex='0'>{props.description[0]}</p>
-        <p className='description' tabIndex='0'>{props.description[1]}</p>
-        <p className='description' tabIndex='0'>{props.description[2]}</p>
+        <h1 className="heading" tabIndex="0">{props.heading}</h1>
+        <p className="description" tabIndex="0">{props.description[0]}</p>
+        <p className="description" tabIndex="0">{props.description[1]}</p>
+        <p className="description" tabIndex="0">{props.description[2]}</p>
     </article>
   );
 }

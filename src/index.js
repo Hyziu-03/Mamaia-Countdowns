36ea888d8e97ffab2bfe6f7a1b1e607824942f43
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-06-26 14:01:09 +0200

    Preparing the stylesheet for production

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './styles/styles.css';
-import App from './components/App.jsx';
-import Home from './components/Home.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./styles/styles.min.css";
+import App from "./components/App.jsx";
+import Home from "./components/Home.jsx";
 
 ReactDOM.render(
-    <Router>
-      <Switch>
-        <Route exact path='/set-countdown' component={App} />
-        <Route exact path='/' component={Home} />
-      </Switch>
-    </Router>,
-  document.getElementById('root')
+  <Router>
+    <Switch>
+      <Route exact path="/set-countdown" component={App} />
+      <Route exact path="/" component={Home} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
 );
 
 reportWebVitals();

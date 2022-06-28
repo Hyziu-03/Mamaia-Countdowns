dac92ff757b97bfe50f72dc0dd0be10a705b7a4a
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-06-28 14:23:24 +0200

    Updating the code to follow conventions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./styles/styles.min.css";
+import reportWebVitals from "./reportWebVitals";
 import App from "./components/App.jsx";
 import Home from "./components/Home.jsx";
 

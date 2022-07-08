b45ded54da41a77e19a7970050829d386ea614db
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-07-08 14:45:18 +0200

    Styling event description

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./styles/styles.min.css";
+import "./styles/styles.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./components/App.jsx";
 import Home from "./components/Home.jsx";

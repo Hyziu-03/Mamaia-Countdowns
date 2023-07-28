819af9219a4480b062d16b023be9011b963fdc82
Szymon Hyziak <szymon.hyziak@protonmail.com>
2023-07-28 18:10:36 +0200

    Handling errors in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,20 +2,31 @@
 const CACHE_NAME = 'mamaia-cache';
 
 self.addEventListener('fetch', event => {
-    if (!(event.request.url.indexOf('http') === 0)) return;
+    try {
+        const reqestIsFromExtension = 
+            event.request.url.startsWith("chrome-extension") || 
+            event.request.url.includes("extension");
+        const requestUsesHttp = 
+            !(event.request.url.indexOf("http") === 0);
+        if (reqestIsFromExtension || requestUsesHttp) return;
+    } catch (error) {
+        console.error("Error processing request");
+    }
+    
     event.respondWith(
         fetch(event.request)
-        .then(result => {
-            const resultClone = result.clone();
-            caches
-                .open(CACHE_NAME)
-                .then(cache => cache.put(event.request, resultClone));
-            return result;
-        }).catch(error => {
-            caches
-                .match(event.request)
-                .then(result => result);
-        })
+            .then(result => {
+                const resultClone = result.clone();
+                caches
+                    .open(CACHE_NAME)
+                    .then(cache => cache.put(event.request, resultClone));
+                return result;
+            }).catch(error => {
+                caches
+                    .match(event.request)
+                    .then(result => result);
+                console.error("Error fetching cache");
+            })
     );
 });
 
@@ -23,14 +34,16 @@ self.addEventListener('activate', event => {
     const cacheWhitelist = [];
     cacheWhitelist.push(CACHE_NAME);
     event.waitUntil(
-        caches.keys()
-        .then(cacheNames => Promise.all(
-            // eslint-disable-next-line array-callback-return
-            cacheNames.map(cacheName => {
+        caches
+        .keys()
+            .then(cacheNames =>
+            Promise.all(
+                // eslint-disable-next-line array-callback-return
+                cacheNames.map((cacheName) => {
                 if (!cacheWhitelist.includes(cacheName))
                     return caches.delete(cacheName);
-            })
-        ))
-        .catch(error => console.error(error))
+                })
+            ))
+            .catch((error) => console.error("Error activating cachce"))
     );
 });

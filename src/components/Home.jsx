f945afc9723c4d0ea8158b56f5c6eb44c552688b
Szymon Hyziak <szymon.hyziak@protonmail.com>
2023-08-10 14:08:47 +0200

    Making names consistent

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,8 @@
-import Header from "components/sections/Header.jsx";
-import IntroductorySection from "components/sections/IntroductorySection";
-import DescriptiveSection from "components/sections/DescriptiveSection";
-import AuthorSection from "components/sections/AuthorSection";
-import Footer from "components/sections/Footer";
+import Header from "components/modules/Header.jsx";
+import IntroductorySection from "components/modules/IntroductorySection";
+import DescriptiveSection from "components/modules/DescriptiveSection";
+import AuthorSection from "components/modules/AuthorSection";
+import Footer from "components/modules/Footer";
 
 export default function Home() {
     return (

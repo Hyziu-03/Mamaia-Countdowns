bcf99686dbe66b9cc85b6543d3104adadc4b0f8b
Szymon Hyziak <szymon.hyziak@protonmail.com>
2023-08-24 13:08:00 +0200

    Reimplementing auth

--- README.md
+++ README.md
@@ -5,7 +5,3 @@
 Mamaia Countdowns is a tool to ease your life.
 You can set countdowns for any event you want.
 No matter if it’s Christmas or an important exam to take!
-
-### TASKS
-
-- Reimplement authentication
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+// React
+import { useState } from "react";
 // Firebase
 import { initializeApp } from "firebase/app";
 // Firestore
@@ -6,8 +8,8 @@ import { getFirestore } from "firebase/firestore";
 import { onAuthStateChanged, getAuth } from "firebase/auth";
 // Date
 import { thisYear } from "scripts/date";
-// Firenase utilities
-import { firebaseConfig, id } from "scripts/firebase";
+// Firebase utilities
+import { firebaseConfig } from "scripts/firebase";
 // Other utilities
 import { 
     inspectInputs, 
@@ -26,37 +28,24 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth();
 
-onAuthStateChanged(auth, function (user) {
-    try {
-        user ? console.log("✅ You are logged in!") : awaitRedirect(auth);
-    } catch (error) {
-        console.log("⚠️ Failed to determine login state");
-    }
-});
-
 export default function App() {
+    let [status, setStatus] = useState("");
+
+    onAuthStateChanged(auth, function (user) {
+        try {
+            user ? setStatus("✅") : setStatus("⚠️") && awaitRedirect(auth);
+        } catch (error) {
+            console.log("⚠️ Failed to determine login state");
+        }
+    });
+
     return (
         <section className="mobile-container">
             <Dialog type="text" />
             <Dialog type="login" />
-            <div className="login-dialog-container">
-                <dialog id="login-dialog" className="login-dialog">
-                    <p className="login-dialog-text">
-                        You need to authenticate to add your events 
-                        to Mamaia Countdowns.
-                    </p>
-                    <p className="login-dialog-text">
-                        Please, click the button below.
-                    </p>
-                    <form method="dialog">
-                        <button className="dialog-close btn">
-                            Login with Google
-                        </button>
-                    </form>
-                </dialog>
-            </div>
+            <Dialog type="success" />
             <header className="fixed-header">
-                <Name />
+                <Name /> <span className="status">{status}</span>
             </header>
             <main className="mobile-content">
                 <article className="contact-form-container">
@@ -66,7 +55,7 @@ export default function App() {
                         secondInputType="date"
                         thirdInput="What additional information do you have?"
                     />
-                    <span onClick={() => inspectInputs(db, id)}>
+                    <span onClick={() => inspectInputs(db, auth.currentUser.uid)}>
                         <Button message="Set a Countdown!" id="app-form-btn" />
                     </span>
                 </article>
@@ -85,7 +74,9 @@ export default function App() {
                     </p>
                     <section 
                         className="btn-container" 
-                        onClick={() => verifyLoginState(auth)}
+                        onClick={() => verifyLoginState(
+                            auth, auth.currentUser.uid
+                        )}
                     >
                         <Button message="Pull data from the database" />
                     </section>

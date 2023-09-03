9da3dc67a76b7860d75bd81703978b40f523f991
Szymon Hyziak <szymon.hyziak@protonmail.com>
2023-09-03 12:13:56 +0200

    Ensuring discernible link names and proper scaling

--- src/components/modules/Footer.jsx
+++ src/components/modules/Footer.jsx
@@ -22,23 +22,37 @@ export default function Footer() {
                     href="https://www.linkedin.com/in/szymon-hyziak/"
                     target="_blank"
                     rel="noreferrer noopener"
+                    className="footer-icon"
                 >
                     <LinkedInIcon />
+                    <span className="icon-name">LinkedIn</span>
                 </a>
                 <a
                     href="https://github.com/Hyziu-03/"
                     target="_blank"
                     rel="noreferrer noopener"
+                    className="footer-icon"
                 >
                     <GitHubIcon />
+                    <span className="icon-name">GitHub</span>
                 </a>
                 <a href="mailto:szymonhyziak@student.agh.edu.pl"
                     target="_blank"
                     rel="noreferrer noopener"
+                    className="footer-icon"
                 >
                     <MailIcon />
+                    <span className="icon-name">Mail</span>
                 </a>
-                <BuyMeCoffee />
+                <a
+                    href="https://www.buymeacoffee.com/szymonhyziak"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    className="footer-icon"
+                >
+                    <BuyMeCoffee />
+                    <span className="icon-name">Buy me a coffee</span>
+                </a>
             </section>
         </footer>
     );

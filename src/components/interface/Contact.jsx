1f8dab5aa8f154bebe529fe907492b38ceadff2b
Hyziu-03 <szymon.hyziak@protonmail.com>
2026-05-28 15:00:41 +0200

    chore: add form field label

--- src/components/interface/Contact.jsx
+++ src/components/interface/Contact.jsx
@@ -8,22 +8,26 @@ export default function Contact(props) {
             id="form"
             onSubmit={event => event.preventDefault()}
         >
-            <input
-                type="text"
-                name="name"
-                id="text-input"
-                className="input touch-target text-input"
-                placeholder={firstInput}
-                maxLength="20" 
-            />
-            <input
-                type={secondInputType}
-                name="email"
-                id="email"
-                className="input touch-target email"
-                placeholder={secondInput}
-                maxLength="40" 
-            />
+            <label for="text-input">
+                <input
+                    type="text"
+                    name="name"
+                    id="text-input"
+                    className="input touch-target text-input"
+                    placeholder={firstInput}
+                    maxLength="20" 
+                />
+            </label>
+            <label for="email">
+                <input
+                    type={secondInputType}
+                    name="email"
+                    id="email"
+                    className="input touch-target email"
+                    placeholder={secondInput}
+                    maxLength="40" 
+                />
+            </label>
             <textarea
                 placeholder={thirdInput}
                 className="input textarea touch-target message"

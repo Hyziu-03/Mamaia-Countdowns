79e4a6149820e9d9369ed2a526cee8b0221c7692
Hyziu-03 <szymon.hyziak@protonmail.com>
2026-05-28 14:32:44 +0200

    chore: add svg accessible name

--- src/components/icons/material/ArrowBack.jsx
+++ src/components/icons/material/ArrowBack.jsx
@@ -5,6 +5,7 @@ export default function ArrowBack() {
         viewBox="0 -960 960 960"
         className="icon"
     >
+        <title>Arrow Back</title>
         <path 
             d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" 
         />
